47076716509c6914998c4045ef6c06e4fce87052
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-26 18:33:49 +0400

    change the App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import Home from './container/Home';
+import Login from './components/Login';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { fetchUser } from './utils/fetchUser';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() =>{
+    const user = fetchUser();
+    if(!user) {
+      navigate("/login");
+    }
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
     </div>
   );
 }
